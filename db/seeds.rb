5d60a986370fa8e6fe1777190c36b28ab9cbcfa7
Kelly Walters <kellywdev@gmail.com>
2018-03-20 23:29:33 +0000

    add categories to seed file, add uploads folder to git ignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Removing existing data'
+Category.destroy_all
+
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+Category.create(name: 'Good for Solo travel')
+Category.create(name: 'Good for Group travel')
+Category.create(name: 'Adventure')
+Category.create(name: 'Romantic')
+
