3680c261cb7fd02b116d1d16c5e782fc1c0615b9
Kelly Walters <kellywdev@gmail.com>
2018-03-20 23:42:16 +0000

    add validations to category and tour models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :tours
+  validates :name, presence: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,5 @@
 class Tour < ApplicationRecord
   belongs_to :category
+  validates :name, :description, :price, :category_id, presence: true
   mount_uploader :image, ImageUploader
 end
