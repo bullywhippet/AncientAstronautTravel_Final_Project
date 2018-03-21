b5db22b9d85b1dfb8d7dd85104d60acfe3776757
Kelly Walters <kellywdev@gmail.com>
2018-03-21 00:25:01 +0000

    create a first tour via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,7 @@ Category.create(name: 'Good for Group travel')
 Category.create(name: 'Adventure')
 Category.create(name: 'Romantic')
 
+Tour.create(name: 'Viracocha Adventure',
+            description: 'Starting at the Gateway of the sun in Pum Punku, you will retrace the steps that Viracocha made when he entered the world and created humanity. The gateway of the sun is carved out of a single piece of a 12-10 granite stone with amazing precision. You will then travel via wormhole to the Ichnaea Nebula where untold adventure await. Trip may last as long as you like, or for eternity.',
+            price: 3400.00,
+            category: Category.find_by(name: 'Adventure'))
