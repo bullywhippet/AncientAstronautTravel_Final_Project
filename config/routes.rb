fafec0a58831172ddb3959ee252be426718cd83b
Kelly Walters <kellywdev@gmail.com>
2018-03-21 00:25:28 +0000

    add tours controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :tours, only: %i[index show]
+
+  root to: 'tours#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
