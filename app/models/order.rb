4f23609a6359bcfc359501d3f15b3f904e07ec55
Kelly Walters <kellywdev@gmail.com>
2018-04-04 15:47:40 +0000

    fix new rubocop errors

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
 
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :tours, through: :order_items
   accepts_nested_attributes_for :order_items, allow_destroy: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,6 +3,6 @@ class Tour < ApplicationRecord # :nodoc:
   validates :name, :description, :price, :category_id, presence: true
   mount_uploader :image, ImageUploader
 
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 end
