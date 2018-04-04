4483fb28e3d123f1a08f151dc4eb5b233315355e
Kelly Walters <kellywdev@gmail.com>
2018-04-04 15:32:40 +0000

    added realtional code to orders and tours

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+  has_many :order_items
+  has_many :tours, through: :order_items
+  accepts_nested_attributes_for :order_items, allow_destroy: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,4 +2,7 @@ class Tour < ApplicationRecord # :nodoc:
   belongs_to :category
   validates :name, :description, :price, :category_id, presence: true
   mount_uploader :image, ImageUploader
+
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
