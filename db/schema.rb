0263e8d758abc38ffb885ef18931f06f4b75e6a5
Kelly Walters <kellywdev@gmail.com>
2018-04-04 15:17:59 +0000

    added orderitem join model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404051244) do
+ActiveRecord::Schema.define(version: 20180404151448) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -62,6 +62,17 @@ ActiveRecord::Schema.define(version: 20180404051244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.decimal "price"
+    t.integer "quantity"
+    t.integer "order_id"
+    t.integer "tour_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["tour_id"], name: "index_order_items_on_tour_id"
   end
 
   create_table "orders", force: :cascade do |t|
